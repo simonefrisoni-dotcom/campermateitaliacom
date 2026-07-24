6cd7112e9797c1a3b538377af471984226d876f4
simonefrisoni-dotcom <simonefrisoni@hotmail.com>
2026-07-24 17:13:16 +0200

    Add Italy-wide camping search UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="campermate-v11";
+const CACHE="campermate-v12";
 const FILES=["/","/index.html","/manifest.webmanifest","/icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).catch(()=>{}));
